420de28ffeac107f15ecf8db16f39808173d6d64
lwod <lwodcurrentmail@gmail.com>
2021-04-08 18:21:51 +0300

    react socket added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
-function App() {
+import io from 'socket.io-client'
+import TextField from '@material-ui/core/TextField'
+
+const socket = io.connect('http://localhost:4000')
+
+const App = () => {
+  
+  const [state, setState] = useState({message:'', name:''})
+  
+  const [chat, setChat] = useState([])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
